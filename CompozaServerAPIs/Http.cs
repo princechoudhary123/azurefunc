bf19f918906752bed977cfaa4c0c81e3b042dfd4
FAREAST\hachoud <hachoud@microsoft.com>
2021-04-11 15:49:51 +0530

    name aded

--- CompozaServerAPIs/Http.cs
+++ CompozaServerAPIs/Http.cs
@@ -26,7 +26,7 @@ namespace CompozaServerAPIs
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "hari om  HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
