97c5c0de77e6791337bb250497910863ce2835e3
FAREAST\hachoud <hachoud@microsoft.com>
2021-04-11 17:07:50 +0530

    yml aded

--- CompozaServerAPIs/Http.cs
+++ CompozaServerAPIs/Http.cs
@@ -26,8 +26,8 @@ namespace CompozaServerAPIs
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "hari om  HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                ? "hari om  yml file addedHTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully. yml file added";
 
             return new OkObjectResult(responseMessage);
         }
