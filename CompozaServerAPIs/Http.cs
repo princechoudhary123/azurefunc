63935573a9852878b6ca2cff55d15a48bf4217b4
FAREAST\hachoud <hachoud@microsoft.com>
2021-04-11 14:03:36 +0530

    new fun added

--- CompozaServerAPIs/Http.cs
+++ CompozaServerAPIs/Http.cs
@@ -31,5 +31,25 @@ namespace CompozaServerAPIs
 
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("HttpNew")]
+        public static async Task<IActionResult> RunNew(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+           ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully. new function";
+
+            return new OkObjectResult(responseMessage);
+        }
     }
 }
